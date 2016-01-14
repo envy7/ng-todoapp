84f4120a45d16c1c014bbce69459df42bb472cb6
Noel <noel.varghese.22@gmail.com>
2016-01-14 19:31:52 +0530

    added functionality for deleting entries

--- app/js/controllers/entries.js
+++ app/js/controllers/entries.js
@@ -1,25 +1,30 @@
 todoApp.controller('EntriesController', 
-	['$scope', '$rootScope', '$firebaseAuth', '$firebaseArray', 'FIREBASE_URL' ,
-	function($scope, $rootScope, $firebaseAuth, $firebaseArray, FIREBASE_URL ) {
-  		
-  		var ref = new Firebase(FIREBASE_URL);
-  		var auth = $firebaseAuth(ref);
+  ['$scope', '$rootScope', '$firebaseAuth', '$firebaseArray', 'FIREBASE_URL' ,
+  function($scope, $rootScope, $firebaseAuth, $firebaseArray, FIREBASE_URL ) {
+      
+      var ref = new Firebase(FIREBASE_URL);
+      var auth = $firebaseAuth(ref);
 
-  		auth.$onAuth(function(authUser){
-  			if(authUser){
-  				var entriesref = new Firebase(FIREBASE_URL + 'users/' + $rootScope.currentUser.$id + '/entries');
-  				var entriesInfo = $firebaseArray(entriesref);
+      auth.$onAuth(function(authUser){
+        if(authUser){
+          var entriesref = new Firebase(FIREBASE_URL + 'users/' + $rootScope.currentUser.$id + '/entries');
+          var entriesInfo = $firebaseArray(entriesref);
+          $scope.entries = entriesInfo;
 
-  				$scope.addEntry = function(){
-  					entriesInfo.$add({
-  						heading: $scope.entryheading,
-  						details: $scope.entrydetails,
-  						date: Firebase.ServerValue.TIMESTAMP
-  					}).then(function(){
-  						$scope.entryheading= '';
-  						$scope.entrydetails= '';
-  					});//promise
-  				};//addentry
-  			}//userAuthenticated
-  		});//on auth		
+          $scope.addEntry = function(){
+            entriesInfo.$add({
+              heading: $scope.entryheading,
+              details: $scope.entrydetails,
+              date: Firebase.ServerValue.TIMESTAMP
+            }).then(function(){
+              $scope.entryheading= '';
+              $scope.entrydetails= '';
+            });//promise
+          };//addentry
+
+          $scope.deleteEntry = function(key){
+            entriesInfo.$remove(key);
+          };//deleteEntry
+        }//userAuthenticated
+      });//on auth    
 }]);//controller
