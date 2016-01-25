ec88ee9c060847dfd3c5e603da8e8b16ee9aecd4
Noel <noel.varghese.22@gmail.com>
2016-01-25 10:30:45 +0530

    added functionality for adding the searched users

--- app/js/controllers/contacts.js
+++ app/js/controllers/contacts.js
@@ -7,10 +7,32 @@ todoApp.controller('ContactsController',
 
       	auth.$onAuth(function(authUser){
         if(authUser){
+          //for searching contacts
           var usersref = new Firebase(FIREBASE_URL + 'users/');
           var usersInfo = $firebaseArray(usersref);
           $scope.users = usersInfo;
 
+          //for adding contacts
+          var contactsref = new Firebase(FIREBASE_URL + 'users/' + $rootScope.currentUser.$id + '/contacts');
+          var contactsInfo = $firebaseArray(contactsref);
+          $scope.contacts = contactsInfo;
+
+          $scope.addcontacts = function(user){
+            contactsInfo.$add({
+              contactId: user.regUser,
+              firstname: user.firstname,
+              lastname: user.lastname,
+              fullname: user.firstname + ' ' + user.lastname,
+              email:  user.email,
+              profileimgurl: user.profileimgurl,
+              date: Firebase.ServerValue.TIMESTAMP
+            });/*.then(function(){
+              $scope.myForm.$setPristine();
+              $scope.myForm.$setUntouched();
+              $scope.entryheading= '';
+              $scope.entrydetails= '';
+            });promise*/
+          };//addentry
           
         }//userAuthenticated
       });//on auth    
